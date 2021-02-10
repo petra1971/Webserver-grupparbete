4e2fa29b448fc86e8ea8cf61ff08d786ae45445b
John-Rotte <john.roth_95@live.com>
2021-02-10 15:42:58 +0100

    Added hibernate in module-info.java and pom in core

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -2,6 +2,14 @@ module se.group4.core {
     //requires se.group4.spi;
     requires se.group4.fileutils;
 
+    //For hibernate
+    requires java.persistence;
+    requires java.sql;
+    requires net.bytebuddy;
+    requires com.fasterxml.classmate;
+    requires java.xml.bind;
+
+    opens se.group4.core to org.hibernate.orm.core;
 //    requires static lombok;
     //Vart är lombok?
 }
